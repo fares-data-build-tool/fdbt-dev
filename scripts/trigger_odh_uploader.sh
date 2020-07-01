8011ac5ab5f9ab85f06cafe61de3478874eb35c1
Tej Powar <tejwant.powar@infinityworks.com>
2020-07-01 14:01:26 +0100

    added the odh-uploader for emailing

---
+++ scripts/trigger_odh_uploader.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+FILE_NAME=$1
+BUCKET_NAME=fdbt-matching-data-dev
+UNVALIDATED_NETEX_BUCKET=fdbt-unvalidated-netex-data-dev
+MATCHING_DATA_BUCKET=fdbt-matching-data-dev
+
+EVENT_DATA=$(cat $FDBT_ROOT/fdbt-dev/data/s3Events/putEvent.json | sed s/KEY_HERE/$FILE_NAME.json/g | sed s/BUCKET_HERE/$BUCKET_NAME/g)
+
+cd $FDBT_ROOT/repos/fdbt-netex-output/src/odh-uploader/
+
+rm -rf build
+
+NODE_ENV=development UNVALIDATED_NETEX_BUCKET=$UNVALIDATED_NETEX_BUCKET MATCHING_DATA_BUCKET=$MATCHING_DATA_BUCKET ts-node run-local.ts "$EVENT_DATA"
+
